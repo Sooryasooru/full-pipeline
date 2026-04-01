8c36a7857746b4a54588447baaaf9d6f068a03b2
Sooryasooru <soorya0286@gmail.com>
2026-04-01 18:44:51 +0530

    1

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,6 +3,5 @@ from sklearn.preprocessing import StandardScaler
 def scale_data(X_train,X_test):
     scaler = StandardScaler()
     X_train_scaler = scaler.fit_transform(X_train)
-    X_test_scaler = scaler.fit_transform(X_test)
-    return X_train,X_test
-
+    X_test_scaler = scaler.transform(X_test)
+    return X_train_scaler, X_test_scaler
--- src/train.py
+++ src/train.py
@@ -43,3 +43,45 @@ if __name__ == "__main__":
     if os.getenv("CI") != "true":
         while True:
             time.sleep(5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+from src.preprocessing import scale_data
+import joblib
+
+
+def train_model():
+    X,y = load_iris(return_X_y=True)
+    X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42)
+    X_test,X_train = scale_data(X_test,X_train)
+
+    model = LogisticRegression()
+    model.fit(X_train,y_train)
+
+    pred = model.predict(X_test)
+    acc = accuracy_score(y_test,pred)
+
+    joblib.dump(model,"model.pkl")
+
+    return acc
+
+
+if __name__ == "__main__":
+    acc = train_model()
+    print("accurayc",acc)
